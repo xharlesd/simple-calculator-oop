1dc17dc6aaa7186e3101e12b3457d7a8406c0461
xharlesd <bernidocharlesdavid@gmail.com>
2023-05-21 22:37:48 +0800

    add try catch to choose operation function

--- Calculator_class.py
+++ Calculator_class.py
@@ -7,20 +7,23 @@ class Calculator:
     # get numbers
     def get_numbers(self):
         try:
-            self.num1 = float(input(Fore.GREEN + "\033[1m" + "\n\t Enter first number:   \033[0m" + "\033[0;32m"))
-            self.num2 = float(input(Fore.GREEN + "\033[1m" + "\t Enter second number:   \033[0m" + "\033[0;32m"))
+            self.num1 = float(input(Fore.CYAN + "\033[1m" + "\n\t Enter first number:   \033[0m" + "\033[0;32m"))
+            self.num2 = float(input(Fore.CYAN + "\033[1m" + "\t Enter second number:   \033[0m" + "\033[0;32m"))
         except:
             print("ERROR! Invalid input.")
 
     # choose operation
     def choose_operation(self): 
-        self.operation = input("Enter operation to be used: ")
-        match self.operation:
-            case "+": self.result = self.num1 + self.num2
-            case "-": self.result = self.num1 - self.num2
-            case "*": self.result = self.num1 * self.num2
-            case "/": self.result = self.num1 / self.num2
-        return self.result
+        try:
+            self.operation = input("Enter operation to be used: ")
+            match self.operation:
+                case "+": self.result = self.num1 + self.num2
+                case "-": self.result = self.num1 - self.num2
+                case "*": self.result = self.num1 * self.num2
+                case "/": self.result = self.num1 / self.num2
+            return self.result
+        except:
+            print("ERROR! Invalid input.")
     
     # display result
     def display_result(self):
