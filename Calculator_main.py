5730de3732a81a0d5837b2fc9a0f6cb285463c12
xharlesd <bernidocharlesdavid@gmail.com>
2023-05-21 17:54:03 +0800

    test run 1: okay

--- Calculator_main.py
+++ Calculator_main.py
@@ -4,5 +4,10 @@ from Calculator_class import Calculator
 calc = Calculator()
 
 # get numbers
+calc.get_numbers()
+
 # choose operation
+calc.choose_operation()
+
 # display result
+calc.display_result()
