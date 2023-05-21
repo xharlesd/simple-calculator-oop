f04ba1cf0e41729144255677f74ae2bbd855afbe
xharlesd <bernidocharlesdavid@gmail.com>
2023-05-21 17:41:58 +0800

    get numbers

--- Calculator_class.py
+++ Calculator_class.py
@@ -8,5 +8,12 @@ class Calculator:
         self.result = ""
 
     # get numbers
+    def get_numbers(self):
+        try:
+            self.num1 = float(input("Enter first number: "))
+            self.num2 = float(input("Enter second number: "))
+        except:
+            print("ERROR! Invalid input.")
+
     # choose operation
     # display result
