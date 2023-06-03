fac44aa1cbe9cb12199a371bdf94601ce515769d
xharlesd <bernidocharlesdavid@gmail.com>
2023-06-03 19:41:25 +0800

    create try again function in calc child class

--- Calculator_child_class.py
+++ Calculator_child_class.py
@@ -1,4 +1,33 @@
+import pyfiglet
+from colorama import Fore, Style
+import time
 from Calculator_class import Calculator
 
 class CalculatorChild(Calculator):
-    pass
+    def try_again():
+        retry = None
+        while retry is None:
+            time.sleep(1)
+            again = input(Fore.MAGENTA + "\n\t Do you want to try again (YES/NO)?  ")
+
+            # the program will run again if the user inputted YES
+            if again == "Y" or again == "Yes" or again == "YES" or again == "yes":
+                retry = str(again)
+                continue
+
+            # the program will terminate if the user inputted NO
+            if again == "N" or again == "No" or again == "NO" or again == "no":
+                time.sleep(0.5)
+                print(Fore.CYAN + "\t [Program will be terminated..............]")
+
+                time.sleep(1.5)
+                print("\n")
+                print(Fore.CYAN + "\033[1m-" * 110 + '\033[0m')
+                print(" ")
+
+                end = pyfiglet.figlet_format("   THANK YOU !!   ", font = "banner3",  width = 110, justify = "center")
+                print(Style.BRIGHT + Fore.CYAN + end)
+                exit()
+
+            else:
+                print(Fore.RED + "\t [ERROR] Please choose either YES or NO only.")
